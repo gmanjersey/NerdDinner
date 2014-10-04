2b32af1f8f87dae0b112e86b3bf0b9def60b6ced
Leroy Graham <gmanjersey@gmail.com>
2014-10-03 22:50:43 -0400

    Updating Edit Post to save from Request Variables Updating DB file

--- NerdDinner/Controllers/DinnersController.cs
+++ NerdDinner/Controllers/DinnersController.cs
@@ -44,7 +44,28 @@ namespace NerdDinner.Controllers
             return View(dinner);
         }
 
+        //Post /Dinners/Edit/2
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Edit(int id, FormCollection formValues)
+        {
+            
+            //Retrieve existing dinner
+            Dinner dinner = dinnerRepository.GetDinner(id);
 
+            //Update dinner with Request form posted values:
+            dinner.Title = Request.Form["Title"];
+            dinner.Description = Request.Form["Description"];
+            dinner.EventDate = DateTime.Parse(Request.Form["EventDate"]);
+            dinner.Address = Request.Form["Address"];
+            dinner.Country = Request.Form["Country"];
+            dinner.ContactPhone = Request.Form["ContactPhone"];
+
+            //Persist changes back to database
+            dinnerRepository.Save();
+
+            //Perform Http redirect to details page for the saved Dinner
+            return RedirectToAction("Details", new { id = dinner.DinnerId });
+        }
 
 
     }
