2d257560e2e3e3063774b6fcfb60f8d6758c483e
Leroy Graham <gmanjersey@gmail.com>
2014-10-04 00:42:18 -0400

    Complete the Delete View Pages and Controller Logic

--- NerdDinner/Controllers/DinnersController.cs
+++ NerdDinner/Controllers/DinnersController.cs
@@ -104,5 +104,36 @@ namespace NerdDinner.Controllers
            return View(dinner);
         }
 
+        //HTTP GET: /Diners/Delete/1
+        public ActionResult Delete(int id)
+        {
+            Dinner dinner = dinnerRepository.GetDinner(id);
+
+            if (dinner == null)
+                return View("NotFound");
+            else
+
+                return View(dinner);
+        }
+
+        //Http Post: /Dinners/Delete/1
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Delete(int id, string confirmButton)
+        {
+            Dinner dinner = dinnerRepository.GetDinner(id);
+
+            if (dinner == null)
+                return View("NotFound");
+            
+            dinnerRepository.Delete(dinner);
+            dinnerRepository.Save();
+
+            return View("Deleted");
+
+
+        }
+
+
+
     }
 }
